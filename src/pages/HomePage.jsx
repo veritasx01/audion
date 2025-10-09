4ec53c57087e6cd2d42eeaf919513a1e18bda633
veritas <liorgby2@gmail.com>
2025-10-09 22:29:35 +0300

    spotify layout TODO: improve responsivity

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 export function HomePage() {
   return (
     <section className="home">
-      <h1>Home sweet Home</h1>
+      <h1>left view</h1>
+      <h1>main view</h1>
+      <h1>right view</h1>
     </section>
   );
 }
