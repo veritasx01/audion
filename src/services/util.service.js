ecbc6e08f90f39088d2849ad0c916daf00c27b73
veritas <liorgby2@gmail.com>
2025-10-15 00:46:06 +0300

    utils refactor

--- src/services/util.service.js
+++ src/services/util.service.js
@@ -1,8 +1,8 @@
-export function makeId(length = 6) {
+export function makeId(length = 16) {
   var txt = '';
   var possible =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
+    
   for (var i = 0; i < length; i++) {
     txt += possible.charAt(Math.floor(Math.random() * possible.length));
   }
@@ -56,15 +56,13 @@ export function makeLorem(size = 100) {
 export function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export function randomPastTime() {
-  const HOUR = 1000 * 60 * 60;
-  const DAY = 1000 * 60 * 60 * 24;
-  const WEEK = 1000 * 60 * 60 * 24 * 7;
-
-  const pastTime = getRandomIntInclusive(HOUR, WEEK);
+  const hour = 1000 * 60 * 60;
+  const week = 1000 * 60 * 60 * 24 * 7;
+  const pastTime = getRandomIntInclusive(hour, week);
   return Date.now() - pastTime;
 }
 
