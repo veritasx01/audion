2af9ffc799f0361c6715dbe1bcdf9e6a17d674f1
veritas <liorgby2@gmail.com>
2025-10-15 01:16:19 +0300

    faker dummy songs

---
+++ src/services/song.service.js
@@ -0,0 +1,19 @@
+import { makeId } from './util.service';
+import { faker } from '@faker-js/faker';
+
+export function createDummySongs(amount = 10) {
+  const songs = [];
+  for (let i = 0; i < amount; i++) {
+    songs.push({
+      _id: makeId(),
+      title: faker.music.songName(),
+      artist: faker.person.fullname,
+      duration: 0,
+      albumName: faker.music.album(),
+      releaseDate: new Date(),
+    });
+  }
+  return songs;
+}
+
+window.cds = createDummySongs();
