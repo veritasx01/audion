b5186013f79703bd569ceb39762550f3904fd144
Chanan Welt <chwelt88@gmail.com>
2025-10-16 14:47:01 +0300

    add an additional export to expose the service as an object with each utility function available as a separate property under the imported util service object

--- src/services/util.service.js
+++ src/services/util.service.js
@@ -1,8 +1,18 @@
+export const utilService = {
+  makeId,
+  makeLorem,
+  getRandomIntInclusive,
+  randomPastTime,
+  debounce,
+  saveToStorage,
+  loadFromStorage,
+};
+
 export function makeId(length = 16) {
-  var txt = '';
+  var txt = "";
   var possible =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
   for (var i = 0; i < length; i++) {
     txt += possible.charAt(Math.floor(Math.random() * possible.length));
   }
@@ -12,43 +22,43 @@ export function makeId(length = 16) {
 
 export function makeLorem(size = 100) {
   var words = [
-    'The sky',
-    'above',
-    'the port',
-    'was',
-    'the color of television',
-    'tuned',
-    'to',
-    'a dead channel',
-    '.',
-    'All',
-    'this happened',
-    'more or less',
-    '.',
-    'I',
-    'had',
-    'the story',
-    'bit by bit',
-    'from various people',
-    'and',
-    'as generally',
-    'happens',
-    'in such cases',
-    'each time',
-    'it',
-    'was',
-    'a different story',
-    '.',
-    'It',
-    'was',
-    'a pleasure',
-    'to',
-    'burn',
+    "The sky",
+    "above",
+    "the port",
+    "was",
+    "the color of television",
+    "tuned",
+    "to",
+    "a dead channel",
+    ".",
+    "All",
+    "this happened",
+    "more or less",
+    ".",
+    "I",
+    "had",
+    "the story",
+    "bit by bit",
+    "from various people",
+    "and",
+    "as generally",
+    "happens",
+    "in such cases",
+    "each time",
+    "it",
+    "was",
+    "a different story",
+    ".",
+    "It",
+    "was",
+    "a pleasure",
+    "to",
+    "burn",
   ];
-  var txt = '';
+  var txt = "";
   while (size > 0) {
     size--;
-    txt += words[Math.floor(Math.random() * words.length)] + ' ';
+    txt += words[Math.floor(Math.random() * words.length)] + " ";
   }
   return txt;
 }
