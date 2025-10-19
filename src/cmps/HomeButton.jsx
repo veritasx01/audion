33d5a3ee1bb6b9da65c065ee50f89cd8031f210d
Chanan Welt <chwelt88@gmail.com>
2025-10-19 12:38:15 +0300

    extract home button svg to assets

--- src/cmps/HomeButton.jsx
+++ src/cmps/HomeButton.jsx
@@ -1,4 +1,6 @@
 import { useLocation, useNavigate } from "react-router";
+import homeFilledIcon from "../assets/icons/home-filled.svg";
+import homeOutlineIcon from "../assets/icons/home-outline.svg";
 
 export function HomeButton() {
   const path = useLocation().pathname;
@@ -12,9 +14,7 @@ export function HomeButton() {
     return (
       <button className="home-button hov-enlarge">
         <span className="size-24">
-          <svg aria-hidden="true" viewBox="0 0 24 24" fill="white">
-            <path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732z"></path>
-          </svg>
+          <img src={homeFilledIcon} alt="Home icon" />
         </span>
       </button>
     );
@@ -23,9 +23,7 @@ export function HomeButton() {
   return (
     <button className="home-button hov-enlarge" onClick={goHome}>
       <span className="size-24">
-        <svg aria-hidden="true" viewBox="0 0 24 24" fill="#a0a0a0">
-          <path d="M12.5 3.247a1 1 0 0 0-1 0L4 7.577V20h4.5v-6a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v6H20V7.577zm-2-1.732a3 3 0 0 1 3 0l7.5 4.33a2 2 0 0 1 1 1.732V21a1 1 0 0 1-1 1h-6.5a1 1 0 0 1-1-1v-6h-3v6a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.577a2 2 0 0 1 1-1.732z"></path>
-        </svg>
+        <img src={homeOutlineIcon} alt="Home icon" />
       </span>
     </button>
   );
