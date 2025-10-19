2fae716f1289f3746f471ef23fa28575a663338d
Chanan Welt <chwelt88@gmail.com>
2025-10-19 13:06:06 +0300

    extract volume bar svg icons to assets

--- src/cmps/VolumeBar.jsx
+++ src/cmps/VolumeBar.jsx
@@ -1,6 +1,9 @@
 import { useState, useRef, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { updateVolume } from "../store/actions/song.action";
+import lowVolumeIcon from "../assets/icons/volume-low.svg";
+import midVolumeIcon from "../assets/icons/volume-mid.svg";
+import highVolumeIcon from "../assets/icons/volume-high.svg";
 
 export function VolumeBar() {
   const [volume, setVolume] = useState(100);
@@ -69,23 +72,10 @@ export function VolumeBar() {
 
 function getCurrentVolSvg(volume) {
   if (volume > 66) {
-    return (
-      <svg viewBox="0 0 16 16" fill="#b2b2b2">
-        <path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.64 3.64 0 0 1-1.33-4.967 3.64 3.64 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.14 2.14 0 0 0 0 3.7l5.8 3.35V2.8zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88"></path>
-        <path d="M11.5 13.614a5.752 5.752 0 0 0 0-11.228v1.55a4.252 4.252 0 0 1 0 8.127z"></path>
-      </svg>
-    );
+    return <img src={highVolumeIcon} alt="High volume icon" />;
   } else if (volume > 33) {
-    return (
-      <svg viewBox="0 0 16 16" fill="#b2b2b2">
-        <path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.64 3.64 0 0 1-1.33-4.967 3.64 3.64 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.14 2.14 0 0 0 0 3.7l5.8 3.35V2.8zm8.683 6.087a4.502 4.502 0 0 0 0-8.474v1.65a3 3 0 0 1 0 5.175z"></path>
-      </svg>
-    );
+    return <img src={midVolumeIcon} alt="Medium volume icon" />;
   } else {
-    return (
-      <svg viewBox="0 0 16 16" fill="#b2b2b2">
-        <path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.64 3.64 0 0 1-1.33-4.967 3.64 3.64 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.14 2.14 0 0 0 0 3.7l5.8 3.35V2.8zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88"></path>
-      </svg>
-    );
+    return <img src={lowVolumeIcon} alt="Low volume icon" />;
   }
 }
