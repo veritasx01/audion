f13f5aa8cf053821cad2b30c15e40903974b907f
veritas <liorgby2@gmail.com>
2025-10-19 07:27:43 +0300

    progress bar tick

--- src/cmps/SongProgress.jsx
+++ src/cmps/SongProgress.jsx
@@ -1,18 +1,39 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 export function SongProgress() {
-  const [progress, SetProgress] = useState(50);
+  const [progress, setProgress] = useState(0);
   const [dragging, setDragging] = useState(false);
   const progressRef = useRef(null);
   const duration = useSelector((state) => state.songModule.currentDuration);
+  const isPlaying = useSelector((state) => state.songModule.isPlaying);
+
+  useEffect(() => {
+    if (!isPlaying) return;
+    let start = Date.now();
+    let initialProgress = progress;
+
+    const tick = () => {
+      const elapsed = (Date.now() - start) / 1000;
+      const newProgress = Math.min(
+        (elapsed * 100) / duration + initialProgress,
+        100
+      );
+      setProgress(newProgress);
+      if (newProgress < 100 && isPlaying) {
+        requestAnimationFrame(tick);
+      }
+    };
+
+    requestAnimationFrame(tick);
+  }, [isPlaying, duration, progress]);
 
   const updateVolumeFromClientX = (clientX) => {
     if (!progressRef.current) return;
     const rect = progressRef.current.getBoundingClientRect();
     let newPos = ((clientX - rect.left) / rect.width) * 100;
     newPos = Math.max(0, Math.min(100, newPos));
-    SetProgress(newPos);
+    setProgress(newPos);
   };
 
   const handleMouseDown = (e) => {
@@ -32,9 +53,12 @@ export function SongProgress() {
     window.addEventListener("mouseup", onUp);
   };
 
+  const secsPassed = (progress * duration) / 100;
   return (
     <div className="progress-container">
-      <div className="playback-position">00:00</div>
+      <div className="playback-position">
+        {Math.floor(secsPassed / 60)}:{Math.floor(secsPassed % 60)}
+      </div>
       <div className="progress-bar-container" onMouseDown={handleMouseDown}>
         <div className="progress-bar" ref={progressRef}>
           <div className="bar-container">
