2545a1502a76be80da7b6f3f6a36d0954278ddd6
veritas <liorgby2@gmail.com>
2025-10-20 08:31:31 +0300

    re-add inline svg because an embedded svg doesn't have hover effects

--- src/cmps/SongWidget.jsx
+++ src/cmps/SongWidget.jsx
@@ -1,4 +1,3 @@
-import addIcon from "../assets/icons/add.svg";
 export function SongWidget() {
   return (
     <div className="song-controls-container">
@@ -12,7 +11,10 @@ export function SongWidget() {
       <div style={{ height: "32px", justifySelf: "center", margin: "0 8px" }}>
         <button className="add-button">
           <span className="size-32" aria-hidden="true">
-            <img src={addIcon} alt="Add icon" />
+            <svg aria-hidden="true" viewBox="0 0 16 16" fill="#b0b0b0">
+              <path d="M8 1.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8"></path>
+              <path d="M11.75 8a.75.75 0 0 1-.75.75H8.75V11a.75.75 0 0 1-1.5 0V8.75H5a.75.75 0 0 1 0-1.5h2.25V5a.75.75 0 0 1 1.5 0v2.25H11a.75.75 0 0 1 .75.75"></path>
+            </svg>
           </span>
         </button>
       </div>
--- src/cmps/VolumeBar.jsx
+++ src/cmps/VolumeBar.jsx
@@ -1,10 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { updateVolume } from "../store/actions/song.action";
-import noVolumeIcon from "../assets/icons/volume-none.svg";
-import lowVolumeIcon from "../assets/icons/volume-low.svg";
-import midVolumeIcon from "../assets/icons/volume-mid.svg";
-import highVolumeIcon from "../assets/icons/volume-high.svg";
 
 export function VolumeBar() {
   const [volume, setVolume] = useState(100);
@@ -73,12 +69,30 @@ export function VolumeBar() {
 
 function getCurrentVolSvg(volume) {
   if (volume > 66) {
-    return <img src={highVolumeIcon} alt="High volume icon" />;
+    return (
+      <svg viewBox="0 0 16 16" fill="#b2b2b2">
+        <path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.64 3.64 0 0 1-1.33-4.967 3.64 3.64 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.14 2.14 0 0 0 0 3.7l5.8 3.35V2.8zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88"></path>
+        <path d="M11.5 13.614a5.752 5.752 0 0 0 0-11.228v1.55a4.252 4.252 0 0 1 0 8.127z"></path>
+      </svg>
+    );
   } else if (volume > 33) {
-    return <img src={midVolumeIcon} alt="Medium volume icon" />;
+    return (
+      <svg viewBox="0 0 16 16" fill="#b2b2b2">
+        <path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.64 3.64 0 0 1-1.33-4.967 3.64 3.64 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.14 2.14 0 0 0 0 3.7l5.8 3.35V2.8zm8.683 6.087a4.502 4.502 0 0 0 0-8.474v1.65a3 3 0 0 1 0 5.175z"></path>
+      </svg>
+    );
   } else if (volume > 0) {
-    return <img src={lowVolumeIcon} alt="Low volume icon" />;
+    return (
+      <svg viewBox="0 0 16 16" fill="#b2b2b2">
+        <path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.64 3.64 0 0 1-1.33-4.967 3.64 3.64 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.14 2.14 0 0 0 0 3.7l5.8 3.35V2.8zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88"></path>
+      </svg>
+    );
   } else {
-    return <img src={noVolumeIcon} alt="no volume icon" />;
+    return (
+      <svg viewBox="0 0 16 16" fill="#b2b2b2">
+        <path d="M13.86 5.47a.75.75 0 0 0-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 0 0 8.8 6.53L10.269 8l-1.47 1.47a.75.75 0 1 0 1.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 0 0 1.06-1.06L12.39 8l1.47-1.47a.75.75 0 0 0 0-1.06"></path>
+        <path d="M10.116 1.5A.75.75 0 0 0 8.991.85l-6.925 4a3.64 3.64 0 0 0-1.33 4.967 3.64 3.64 0 0 0 1.33 1.332l6.925 4a.75.75 0 0 0 1.125-.649v-1.906a4.7 4.7 0 0 1-1.5-.694v1.3L2.817 9.852a2.14 2.14 0 0 1-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694z"></path>
+      </svg>
+    );
   }
 }
