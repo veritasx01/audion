3f5e394469cd5e38e04516b43389fad2ddb73a73
veritas <liorgby2@gmail.com>
2025-10-20 11:53:18 +0300

    demo home page (very crude)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,31 @@
+import { useDispatch } from "react-redux";
+import { songs } from "../assets/data/songs";
+import { updateCurrentSong } from "../store/actions/song.action";
+
 export function HomePage() {
   return (
     <>
-      {}
+      <SongList songs={songs} />
+    </>
+  );
+}
+
+function SongList({ songs }) {
+  const dispatch = useDispatch();
+  const changeToSong = (url) => {
+    dispatch(updateCurrentSong(url));
+  };
+
+  return (
+    <>
+      {songs.map((pl) => (
+        <div key={1}>
+          <p key={1}>{pl.title}</p>
+          <button onClick={() => changeToSong(pl.url)}>
+            change player to song
+          </button>
+        </div>
+      ))}
     </>
   );
 }
