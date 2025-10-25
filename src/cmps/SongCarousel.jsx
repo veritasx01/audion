e8fba4f590e9ec33bec563f5362ed52e154a830e
veritas <liorgby2@gmail.com>
2025-10-25 18:48:27 +0300

    simple carousel (styling changes needed)

--- src/cmps/SongCarousel.jsx
+++ src/cmps/SongCarousel.jsx
@@ -1,18 +1,31 @@
-export function SongCarousel({ songs, title="title" }) {
+import { useState } from "react";
+
+export function SongCarousel({ songs, title = "title" }) {
+  const [offset, setOffset] = useState(0);
+  const moveLeft = () => setOffset((prev) => Math.max(0, prev - 3));
+  const moveRight = () =>
+    setOffset((prev) => Math.min(songs.length - 6, prev + 3));
   return (
-    <div className="song-carousel-container">
+    <div style={{ paddingLeft: "28px", overflow: "hidden" }}>
       <h1 className="carousel-title">{title}</h1>
-      <div className="song-carousel">
-        {songs.map((song, idx) => (
-          <div key={idx} className="song-card">
-            <div style={{width: "100%"}}>
-              <img src={song.thumbnail} alt={song.title} />
-              <p className="card-title">{song.title}</p>
-              <p className="card-artist">{song.artist}</p>
+      <div
+        className="song-carousel-container"
+        style={{ transform: `translate(-${offset * 200}px)` }}
+      >
+        <div className="song-carousel">
+          {songs.map((song, idx) => (
+            <div key={idx} className="song-card">
+              <div style={{ width: "100%" }}>
+                <img src={song.thumbnail} alt={song.title} />
+                <p className="card-title">{song.title}</p>
+                <p className="card-artist">{song.artist}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
+      <button onClick={moveLeft}>-</button>
+      <button onClick={moveRight}>+</button>
     </div>
   );
 }
