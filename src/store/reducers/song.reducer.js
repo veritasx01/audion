2b496f231a77abc131ab8db923fd45c3ab2326e0
veritas <liorgby2@gmail.com>
2025-11-04 02:24:24 +0200

    add new state to song player

--- src/store/reducers/song.reducer.js
+++ src/store/reducers/song.reducer.js
@@ -3,12 +3,18 @@ export const SET_SONG_OBJ = 'SET_SONG_OBJ';
 export const TOGGLE_PLAYING = 'TOGGLE_PLAYING';
 export const SET_VOLUME = 'SET_VOLUME';
 export const SET_DURATION = 'SET_DURATION';
+export const SET_SECS = 'SET_SECS';
+export const SET_ENDED = 'SET_ENDED';
+export const SET_IS_READY = 'SET_IS_READY';
 
 const initialState = {
   currentSong: null,
   songObj: { secs: 0, ended: false },
+  secs: 0, // progression of the audio in secs (can be float)
+  hasEnded: false, // 
+  isReady: false,
   isPlaying: false,
-  currentDuration: 0,
+  currentDuration: 0, // full duration of audio
   volume: 1,
 };
 
@@ -24,6 +30,12 @@ export function songReducer(state = initialState, action) {
       return { ...state, currentDuration: action.payload };
     case SET_SONG_OBJ:
       return { ...state, songObj: { ...state.songObj, ...action.payload } };
+    case SET_SECS:
+      return { ...state, secs: action.payload };
+    case SET_ENDED:
+      return { ...state, hasEnded: action.payload };
+    case SET_IS_READY:
+      return { ...state, isReady: action.payload };
     default:
       return state;
   }
