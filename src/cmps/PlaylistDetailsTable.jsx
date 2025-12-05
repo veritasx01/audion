e742ed4c656e46896f31a3003711c0d01f365d3c
Chanan Welt <chwelt88@gmail.com>
2025-12-06 01:03:20 +0200

    update test condition to include playlist id comparison when comparing currently played song on queue with the song on listed/hovered on playlist details table

--- src/cmps/PlaylistDetailsTable.jsx
+++ src/cmps/PlaylistDetailsTable.jsx
@@ -35,6 +35,9 @@ export function PlaylistDetailsTable({ playlist, loadPlaylist }) {
   const [focusedRow, setFocusedRow] = useState(null);
   const isPlaying = useSelector((store) => store.songModule.isPlaying);
   const playingSongId = useSelector((store) => store.songModule.songObj._id);
+  const playingPlaylistId = useSelector(
+    (store) => store.songQueueModule.playlistId
+  );
   const playlists = useSelector((store) => store.playlistModule.playlists);
   const { contextMenu, showContextMenu, hideContextMenu } = useContextMenu();
 
@@ -188,7 +191,9 @@ export function PlaylistDetailsTable({ playlist, loadPlaylist }) {
             >
               <td className={"song-number-col"} key="num">
                 {hoveredRow === idx ? (
-                  playingSongId === song._id && isPlaying ? (
+                  playingPlaylistId === playlist._id &&
+                  playingSongId === song._id &&
+                  isPlaying ? (
                     <button
                       className="song-play-pause-btn"
                       onClick={() => dispatch(togglePlaying())}
@@ -200,9 +205,15 @@ export function PlaylistDetailsTable({ playlist, loadPlaylist }) {
                     <button
                       className="song-play-pause-btn"
                       onClick={() => {
-                        if (playingSongId === song._id && !isPlaying) {
+                        if (
+                          // if clicking play on the currently loaded song but it's paused, just toggle play
+                          playingPlaylistId === playlist._id &&
+                          playingSongId === song._id &&
+                          !isPlaying
+                        ) {
                           dispatch(togglePlaying());
                         } else {
+                          // otherwise, load the song queue starting from this song
                           dispatch(setSongQueue([...playlist.songs]));
                           dispatch(setPlaylistId(playlist._id));
                           dispatch(seekSongQueueIndex(idx));
@@ -213,14 +224,19 @@ export function PlaylistDetailsTable({ playlist, loadPlaylist }) {
                       {playIcon({})}
                     </button>
                   )
-                ) : playingSongId === song._id && isPlaying ? (
+                ) : playingPlaylistId === playlist._id &&
+                  playingSongId === song._id &&
+                  isPlaying ? (
                   <span className="now-playing-animation" title="Now Playing">
                     {nowPlayingBarChartIcon({})}
                   </span>
                 ) : (
                   <span
                     className={`song-number-col ${
-                      playingSongId === song._id ? "active" : ""
+                      playingPlaylistId === playlist._id &&
+                      playingSongId === song._id
+                        ? "active"
+                        : ""
                     }`}
                   >
                     {idx + 1}
@@ -237,7 +253,10 @@ export function PlaylistDetailsTable({ playlist, loadPlaylist }) {
                   <div className="playlist-song-text">
                     <span
                       className={`playlist-song-title ${
-                        playingSongId === song._id ? "active" : ""
+                        playingPlaylistId === playlist._id &&
+                        playingSongId === song._id
+                          ? "active"
+                          : ""
                       }`}
                     >
                       {song.title}
