fe95593f5dad87a0e04dd48afac12904f2d496a7
veritas <liorgby2@gmail.com>
2025-12-06 20:52:27 +0200

    now playing view with add songs to liked button(only visually functional)

--- src/cmps/NowPlayingView.jsx
+++ src/cmps/NowPlayingView.jsx
@@ -1,23 +1,10 @@
-import fallbackImage from "../assets/images/black_image.jpg";
+import { NowPlayingDetails } from "./NowPlayingDetails";
 
 export function NowPlayingView({ songObj }) {
   return (
     <div>
       <p className="now-playing-artist-header">{songObj?.artist}</p>
       <NowPlayingDetails songObj={songObj}></NowPlayingDetails>
-    </div>
-  );
-}
-
-function NowPlayingDetails({ songObj }) {
-  return (
-    <div className="now-playing-details-container">
-      <img
-        className="now-playing-image"
-        src={songObj?.thumbnail || fallbackImage}
-      ></img>
-      <p className="now-playing-title">{songObj?.title}</p>
-      <p className="now-playing-artist">{songObj?.artist}</p>
     </div>
   );
 }
