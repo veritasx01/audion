f7d4b7043d6bf76874b9ac54d3fd13650d2dc613
Chanan Welt <chwelt88@gmail.com>
2025-12-08 04:33:40 +0200

    split liked songs from user library store playlists into a different standalone variable for easier use through the app

--- src/store/reducers/userLibrary.reducer.js
+++ src/store/reducers/userLibrary.reducer.js
@@ -1,7 +1,7 @@
+export const SET_IS_LOADING = "SET_IS_LOADING";
 export const SET_PLAYLISTS_IN_LIBRARY = "SET_PLAYLISTS_IN_LIBRARY";
 export const REMOVE_PLAYLIST_FROM_LIBRARY = "REMOVE_PLAYLIST_FROM_LIBRARY";
 export const ADD_PLAYLIST_TO_LIBRARY = "ADD_PLAYLIST_TO_LIBRARY";
-export const SET_IS_LOADING = "SET_IS_LOADING";
 export const ADD_SONG_TO_LIKED_SONGS = "ADD_SONG_TO_LIKED_SONGS";
 export const REMOVE_SONG_FROM_LIKED_SONGS = "REMOVE_SONG_FROM_LIKED_SONGS";
 
@@ -15,14 +15,26 @@ export const userLibraryActions = {
 };
 
 const initialState = {
-  playlists: [],
+  playlists: [], // Regular playlists (excluding liked songs)
+  likedSongs: null, // The special liked songs playlist
   isLoading: false,
 };
 
 export function userLibraryReducer(state = initialState, action) {
   switch (action.type) {
     case SET_PLAYLISTS_IN_LIBRARY:
-      return { ...state, playlists: action.payload };
+      const allPlaylists = action.payload;
+      const likedSongs =
+        allPlaylists.find((pl) => pl.isLikedSongs === true) || null;
+      const regularPlaylists = allPlaylists.filter(
+        (pl) => pl.isLikedSongs !== true
+      );
+
+      return {
+        ...state,
+        playlists: regularPlaylists,
+        likedSongs: likedSongs,
+      };
     case REMOVE_PLAYLIST_FROM_LIBRARY:
       return {
         ...state,
@@ -39,29 +51,25 @@ export function userLibraryReducer(state = initialState, action) {
     case ADD_SONG_TO_LIKED_SONGS:
       return {
         ...state,
-        playlists: state.playlists.map((playlist) => {
-          if (playlist.isLikedSongs) {
-            return {
-              ...playlist,
-              songs: [...playlist.songs, action.payload],
-            };
-          } else return playlist;
-        }),
+        likedSongs: state.likedSongs
+          ? {
+              ...state.likedSongs,
+              songs: [...state.likedSongs.songs, action.payload],
+            }
+          : null,
       };
 
     case REMOVE_SONG_FROM_LIKED_SONGS:
       return {
         ...state,
-        playlists: state.playlists.map((playlist) => {
-          if (playlist.isLikedSongs) {
-            return {
-              ...playlist,
-              songs: playlist.songs.filter(
+        likedSongs: state.likedSongs
+          ? {
+              ...state.likedSongs,
+              songs: state.likedSongs.songs.filter(
                 (song) => song._id !== action.payload
               ),
-            };
-          } else return playlist;
-        }),
+            }
+          : null,
       };
 
     case SET_IS_LOADING:
