6bbd5a0d13d73f2f1c8ff8e6365e44f0252aea7d
veritas <liorgby2@gmail.com>
2025-12-10 18:46:14 +0200

    carousel variation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,15 +2,23 @@ import { useEffect, useState } from "react";
 import { HomeHeader } from "../cmps/HomeHeader";
 import { SongCarousel } from "../cmps/SongCarousel";
 import { playlistService } from "../services/playlist/playlist.service";
+import { shuffleArray } from "../services/util.service";
 
 export function HomePage() {
   const [playlists, setplaylists] = useState([]);
+  const [lists, setLists] = useState([]);
+
   useEffect(() => {
     const initplaylists = async () => {
-      let playlists = await playlistService.query();
-      console.log(playlists)
-      playlists = playlists.filter((pl) => !pl.isLikedSongs) 
-      setplaylists(playlists ? playlists : []);
+      let playlistsQuery = await playlistService.query();
+      playlistsQuery = playlistsQuery.filter((pl) => !pl.isLikedSongs);
+      setplaylists(playlistsQuery ? playlistsQuery : []);
+      setLists([
+        playlists,
+        shuffleArray(playlists),
+        shuffleArray(playlists),
+        shuffleArray(playlists),
+      ]);
     };
     initplaylists();
   }, []);
@@ -19,11 +27,14 @@ export function HomePage() {
     <>
       <HomeHeader></HomeHeader>
       <div className="content-view">
-        <SongCarousel playlists={playlists} title="Made For You"></SongCarousel>
-        <SongCarousel playlists={playlists} title="Jump back in"></SongCarousel>
-        <SongCarousel playlists={playlists} title="Recently played"></SongCarousel>
+        <SongCarousel playlists={lists[0]} title="Made For You"></SongCarousel>
+        <SongCarousel playlists={lists[1]} title="Jump back in"></SongCarousel>
         <SongCarousel
-          playlists={playlists}
+          playlists={lists[2]}
+          title="Recently played"
+        ></SongCarousel>
+        <SongCarousel
+          playlists={lists[3]}
           title="Recommended for today"
         ></SongCarousel>
       </div>
