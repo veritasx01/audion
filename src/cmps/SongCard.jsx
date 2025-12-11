2a6ce43a04271515b7bc454eb7f6469b3f810b60
veritas <liorgby2@gmail.com>
2025-12-11 23:55:13 +0200

    add go to playlist page when clicking on playlists

--- src/cmps/SongCard.jsx
+++ src/cmps/SongCard.jsx
@@ -1,15 +1,27 @@
 import { useSongController } from "../customHooks/useSongController";
 import { pauseIcon, playIcon } from "../services/icon.service";
+import { useNavigate } from "react-router-dom";
 
 export function SongCard({ playlist }) {
+  const navigate = useNavigate();
   const song = playlist.songs[0];
   //const goToPlaylist = () => {};
 
   const { isCurrentSongPlaying, toggleSong } = useSongController(song);
 
+  const handlePlayButtonClick = (e) => {
+    e.stopPropagation();
+    toggleSong(song);
+  };
+
+  function goToPlaylistPage() {
+    if (!playlist._id || playlist._id === "") return;
+    navigate(`playlist/${playlist._id}`);
+  }
+
   return (
-    <div className="song-card">
-      <button className="play-button-carousel" onClick={() => toggleSong(song)}>
+    <div className="song-card" onClick={goToPlaylistPage}>
+      <button className="play-button-carousel" onClick={handlePlayButtonClick}>
         <span className="size-48">
           {isCurrentSongPlaying
             ? pauseIcon({ height: "24px", width: "24px", fill: "black" })
