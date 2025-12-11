b4a8f361113b98494b18d04664267524a97fee3c
Chanan Welt <chwelt88@gmail.com>
2025-12-11 12:49:26 +0200

    remove demo song generation

--- src/services/song/song.service.js
+++ src/services/song/song.service.js
@@ -6,8 +6,6 @@ import {
   saveToStorage,
   escapeRegexSpecialCharacters,
 } from "../util.service.js";
-import { faker } from "@faker-js/faker";
-import { songs as jsonDemoSongs } from "../../assets/data/songs.js";
 
 export const songService = {
   query,
@@ -18,8 +16,6 @@ export const songService = {
 };
 
 const STORAGE_KEY = "songDB";
-
-_saveDemoSongsToLocalStorage(jsonDemoSongs);
 
 // query playlists from storage with optional filtering by title or description
 export function query(filterBy) {
@@ -68,53 +64,3 @@ export async function getById(songId) {
 //async function add(song) {}
 
 //async function update(songId) {}
-
-function _saveDemoSongsToLocalStorage(songs) {
-  saveToStorage(STORAGE_KEY, songs);
-}
-
-export function _createDummySongs(amount = 10) {
-  const songs = [];
-  const thumbnails = [
-    "https://i.scdn.co/image/ab67616d000048515dc62d661568dcaa3b18ce6a",
-    "https://i.scdn.co/image/ab67616d00004851ae6d8e36136353d550b2587d",
-    "https://i.scdn.co/image/ab67616d000048515074bd0894cb1340b8d8a678",
-    "https://i.scdn.co/image/ab67616d00004851926909699c1214051c7a9937",
-    "https://i.scdn.co/image/ab67616d00004851aaeb5c9fb6131977995b7f0e",
-  ];
-  const genres = [
-    "Rock",
-    "Pop",
-    "Jazz",
-    "Classical",
-    "Hip Hop",
-    "Electronic",
-    "Reggae",
-    "Blues",
-    "Country",
-    "Funk",
-    "Soul",
-    "Metal",
-    "R&B",
-    "Punk",
-    "Disco",
-    "Folk",
-    "Techno",
-    "Ska",
-    "House",
-    "Gospel",
-  ];
-  for (let i = 0; i < amount; i++) {
-    songs.push({
-      _id: makeId(),
-      title: faker.music.songName(),
-      artist: faker.person.fullname,
-      duration: getRandomIntInclusive(5, 15),
-      albumName: faker.music.album(),
-      songThumbnail: getRandomValues(thumbnails, 1)[0],
-      releaseDate: new Date(),
-      genres: [...getRandomValues(genres, 3)],
-    });
-  }
-  return songs;
-}
