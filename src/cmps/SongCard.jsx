f92c6b2d1e2785584f545d062fa7f4cbee0d32da
veritas <liorgby2@gmail.com>
2025-12-12 02:06:13 +0200

    make card play the playlist itself

--- src/cmps/SongCard.jsx
+++ src/cmps/SongCard.jsx
@@ -1,17 +1,44 @@
-import { useSongController } from "../customHooks/useSongController";
+import { useDispatch, useSelector } from "react-redux";
 import { pauseIcon, playIcon } from "../services/icon.service";
+import { useNavigate } from "react-router-dom";
+import {
+  clearSongQueue,
+  setPlaylistId,
+  setSongQueue,
+} from "../store/actions/songQueue.action";
+import { setPlaying, togglePlaying } from "../store/actions/song.action";
 
 export function SongCard({ playlist }) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const song = playlist.songs[0];
+  const playlistId = useSelector((state) => state.songQueueModule.playlistId);
   //const goToPlaylist = () => {};
 
-  const { isCurrentSongPlaying, toggleSong } = useSongController(song);
+  const handlePlayPause = (e) => {
+    e.stopPropagation();
+    // If it's already the current playlist (regardless of play state), just toggle
+    if (playlist._id !== playlistId) {
+      // Load this playlist and start playing
+      dispatch(clearSongQueue());
+      dispatch(setSongQueue([...playlist.songs]));
+      dispatch(setPlaylistId(playlist._id));
+      dispatch(setPlaying(true));
+    } else {
+      dispatch(togglePlaying());
+    }
+  };
+
+  function goToPlaylistPage() {
+    if (!playlist._id || playlist._id === "") return;
+    navigate(`playlist/${playlist._id}`);
+  }
 
   return (
-    <div className="song-card">
-      <button className="play-button-carousel" onClick={() => toggleSong(song)}>
+    <div className="song-card" onClick={goToPlaylistPage}>
+      <button className="play-button-carousel" onClick={handlePlayPause}>
         <span className="size-48">
-          {isCurrentSongPlaying
+          {playlistId === playlist._id
             ? pauseIcon({ height: "24px", width: "24px", fill: "black" })
             : playIcon({ height: "24px", width: "24px", fill: "black" })}
         </span>
