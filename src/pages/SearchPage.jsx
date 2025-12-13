60cedf8a891b52bd004c0b7ab45326c139700784
veritas <liorgby2@gmail.com>
2025-12-13 02:23:09 +0200

    add song carousel to search page for playlists

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,11 +1,31 @@
 import { useParams } from "react-router";
 import { SearchHeader } from "../cmps/SearchHeader";
+import { useEffect, useState } from "react";
+import { playlistService } from "../services/playlist/playlist.service";
+import {SongCarousel} from "../cmps/SongCarousel"
 
 export function SearchPage() {
   const { searchWord } = useParams();
+  const [playlists, setPlaylists] = useState([]);
+
+  useEffect(() => {
+    const loadPlaylists = async () => {
+      let playlistsQuery = await playlistService.query({
+        freeText: searchWord,
+      });
+      playlistsQuery = playlistsQuery.filter((pl) => !pl.isLikedSongs);
+      setPlaylists(playlistsQuery);
+    };
+    loadPlaylists();
+  }, [searchWord]);
+
   return (
     <div>
       <SearchHeader searchWord={searchWord}></SearchHeader>
+      <div className="search-playlists-container">
+        <h2>Playlists</h2>
+        <SongCarousel playlists={playlists.slice(0,5)}></SongCarousel>
+      </div>
     </div>
   );
 }
