e715904c7573ad7425e51898c0105ecac89fab5c
veritas <liorgby2@gmail.com>
2025-12-15 19:39:23 +0200

    fix search page loading

--- src/cmps/SearchHeader.jsx
+++ src/cmps/SearchHeader.jsx
@@ -1,63 +1,11 @@
-import { useEffect, useState } from "react";
 import { SongResultCard } from "./SongResultCard";
-import { songService } from "../services/song/song.service.js";
 import fallbackImage from "../assets/images/black_image.jpg";
 import { playIcon, pauseIcon } from "../services/icon.service.jsx";
 import { useSongController } from "../customHooks/useSongController.jsx";
-import { Loader } from "./Loader.jsx";
 
-const defaultSongs = [
-  {
-    title: "",
-    artist: "",
-    thumbnail: `https://picsum.photos/seed/1/40`,
-    duration: 122,
-  },
-  {
-    title: "",
-    artist: "",
-    thumbnail: `https://picsum.photos/seed/2/40`,
-    duration: 227,
-  },
-  {
-    title: "",
-    artist: "",
-    thumbnail: `https://picsum.photos/seed/3/40`,
-    duration: 183,
-  },
-  {
-    title: "",
-    artist: "",
-    thumbnail: `https://picsum.photos/seed/4/40`,
-    duration: 250,
-  },
-];
-
-export function SearchHeader({ searchWord }) {
-  const [songsResult, setSongsResult] = useState(defaultSongs);
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    // add searching mechanism here
-    const loadSongs = async () => {
-      setIsLoading(true);
-      const songs = await songService.query({ freeText: searchWord });
-      setIsLoading(false);
-      setSongsResult(songs.slice(0, 4));
-    };
-    loadSongs();
-  }, [searchWord]);
-
-  const curSong = songsResult && songsResult.length > 0 ? songsResult[0] : null;
-
+export function SearchHeader({ songs }) {
+  const curSong = songs && songs.length > 0 ? songs[0] : null;
   const { isCurrentSongPlaying, toggleSong } = useSongController(curSong);
-
-  if (isLoading) {
-    return (
-      <div style={{ padding: "100px" }}>
-        <Loader></Loader>
-      </div>
-    );
-  }
 
   return (
     <div className="search-header-container">
@@ -68,9 +16,9 @@ export function SearchHeader({ searchWord }) {
             <div className="top-result-card">
               <img
                 className="result-card-image"
-                src={songsResult[0]?.thumbnail || fallbackImage}
+                src={songs[0]?.thumbnail || fallbackImage}
               ></img>
-              <h1>{songsResult[0]?.title || ""}</h1>
+              <h1>{songs[0]?.title || ""}</h1>
               <div style={{ position: "absolute" }}>
                 <button className="results-play-button" onClick={toggleSong}>
                   <span className="play-button-span">
@@ -92,7 +40,7 @@ export function SearchHeader({ searchWord }) {
           </section>
           <section className="songs-result-section">
             <h2>Songs</h2>
-            {songsResult.map((song) => (
+            {songs.map((song) => (
               <SongResultCard key={song._id} song={song}></SongResultCard>
             ))}
           </section>
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -2,29 +2,41 @@ import { useParams } from "react-router";
 import { SearchHeader } from "../cmps/SearchHeader";
 import { useEffect, useState } from "react";
 import { playlistService } from "../services/playlist/playlist.service";
-import {SongCarousel} from "../cmps/SongCarousel"
+import { SongCarousel } from "../cmps/SongCarousel";
+import { Loader } from "../cmps/Loader.jsx";
+import { songService } from "../services/song/song.service.js";
 
 export function SearchPage() {
   const { searchWord } = useParams();
   const [playlists, setPlaylists] = useState([]);
+  const [songs, setSongs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const loadPlaylists = async () => {
+      setIsLoading(true);
       let playlistsQuery = await playlistService.query({
         freeText: searchWord,
       });
+      let songsQuery = await songService.query({ freeText: searchWord });
       playlistsQuery = playlistsQuery.filter((pl) => !pl.isLikedSongs);
       setPlaylists(playlistsQuery);
+      setSongs(songsQuery);
+      setIsLoading(false);
     };
     loadPlaylists();
   }, [searchWord]);
+  
+  if(isLoading) {
+    return <Loader></Loader>
+  }
 
   return (
     <div>
-      <SearchHeader searchWord={searchWord}></SearchHeader>
+      <SearchHeader songs={songs}></SearchHeader>
       <div className="search-playlists-container">
         <h2>Playlists</h2>
-        {/*<SongCarousel playlists={playlists.slice(0,5)}></SongCarousel>*/}
+        <SongCarousel playlists={playlists.slice(0, 5)}></SongCarousel>
       </div>
     </div>
   );
